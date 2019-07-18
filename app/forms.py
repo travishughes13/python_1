d9c9ecac376a658654155fe6a4f0518daef8a76f
TREX Hughes <TREX@traviss-mbp-457.attlocal.net>
2019-07-17 21:23:23 -0500

    Pushing some form updates

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 #Here I am going to import my Form Classes
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
 #Now I'm going to create a new Class, LoginForm, that passes an argument of FlaskForm and instantiates the required fields
 class LoginForm(FlaskForm):
@@ -9,3 +10,19 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('This username has already been taken. Please use a different one.')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not Noneraise ValidationError('This email address has already been used. Please try a different one.')
