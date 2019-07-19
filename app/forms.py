87ac93821cb8ca1f5c260ea4e054f04fcde5f3cf
TREX Hughes <TREX@traviss-mbp-457.attlocal.net>
2019-07-18 23:05:50 -0500

    checking in with a user profile edit feature

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 #Here I am going to import my Form Classes
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, DataRequired, Length
 from app.models import User
 
 #Now I'm going to create a new Class, LoginForm, that passes an argument of FlaskForm and instantiates the required fields
@@ -27,3 +27,8 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please try a different email address.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
