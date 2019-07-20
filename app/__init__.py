f09ad6543c4399268701ba92652602fdaf61a616
TREX Hughes <TREX@traviss-mbp-457.attlocal.net>
2019-07-19 23:16:49 -0500

    added error handling to prevent duplicated username errors

--- app/__init__.py
+++ app/__init__.py
@@ -44,7 +44,7 @@ if not app.debug:
         app.logger.addHandler(mail_handler)
     
     if not os.path.exists('logs'):
-        os.mkdir('logs'):
+        os.mkdir('logs')
     file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
     file_handler.setFormatter(logging.INFO)
     app.logger.addHandler(file_handler)
