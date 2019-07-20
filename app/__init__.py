1f06f532ee1907646124f47de8f535ad2c58eed6
TREX Hughes <TREX@traviss-mbp-457.attlocal.net>
2019-07-19 22:42:43 -0500

    making sure i back things up

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 #This imports the Flask Login state manager
 from flask_login import LoginManager
+# importing my logging config
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 #This creates my app variable
 app = Flask(__name__)
@@ -21,6 +25,32 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 #This is the migration engine
 migrate = Migrate(app, db)
+# This is for error reporting (via email)
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth=None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='noreply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+    
+    if not os.path.exists('logs'):
+        os.mkdir('logs'):
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 #This imports my routes
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -7,3 +7,10 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Note that none of the below will work until I actually set an email address under the ADMINS config variable
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['you-email@example.com']
