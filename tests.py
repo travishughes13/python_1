5eb72b0186c363c02c05a41e7185df138b00ed2e
TREX Hughes <TREX@traviss-mbp-457.attlocal.net>
2019-07-20 18:12:06 -0500

    found the error in my unit testing file

--- tests.py
+++ tests.py
@@ -55,10 +55,10 @@ class UserModelCase(unittest.TestCase):
 
         # create four posts
         now = datetime.utcnow()
-        p1 = Post(body='post from john', author='u1', timestamp=now + timedelta(seconds=1))
-        p2 = Post(body='post from susan', author='u2', timestamp=now + timedelta(seconds=4))
-        p3 = Post(body='post from mary', author='u3', timestamp=now + timedelta(seconds=3))
-        p4 = Post(body='post from david', author='u4', timestamp=now + timedelta(seconds=2))
+        p1 = Post(body='post from john', author=u1, timestamp=now + timedelta(seconds=1))
+        p2 = Post(body='post from susan', author=u2, timestamp=now + timedelta(seconds=4))
+        p3 = Post(body='post from mary', author=u3, timestamp=now + timedelta(seconds=3))
+        p4 = Post(body='post from david', author=u4, timestamp=now + timedelta(seconds=2))
         db.session.add_all([p1, p2, p3, p4])
         db.session.commit()
 
