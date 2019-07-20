a14b2c2f00b339b304a2f28b4f8412c59f6df275
TREX Hughes <TREX@traviss-mbp-457.attlocal.net>
2019-07-20 17:58:48 -0500

    update

--- app/models.py
+++ app/models.py
@@ -9,7 +9,10 @@ from hashlib import md5
 def load_user(id):
     return User.query.get(int(id))
 
-followers = db.Table('followers', db.Column('follower_id', db.Integer, db.ForeignKey('user.id')), db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,8 +26,7 @@ class User(UserMixin, db.Model):
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
-        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
-    )
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -52,7 +54,9 @@ class User(UserMixin, db.Model):
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
-        followed = Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id)
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
--- tests.py
+++ tests.py
@@ -20,9 +20,7 @@ class UserModelCase(unittest.TestCase):
 
     def test_avatar(self):
         u = User(username='john', email='john@example.com')
-        self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
-                                         'd4c74594d841139328695756648b6bd6'
-                                         '?d=identicon&s=128'))
+        self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6?d=identicon&s=128'))
 
     def test_follow(self):
         u1 = User(username='john', email='john@example.com')
@@ -58,9 +56,9 @@ class UserModelCase(unittest.TestCase):
         # create four posts
         now = datetime.utcnow()
         p1 = Post(body='post from john', author='u1', timestamp=now + timedelta(seconds=1))
-        p2 = Post(body='post from susan', author='u2', timestamp=now + timedelta(seconds=1))
-        p3 = Post(body='post from mary', author='u3', timestamp=now + timedelta(seconds=1))
-        p4 = Post(body='post from david', author='u4', timestamp=now + timedelta(seconds=1))
+        p2 = Post(body='post from susan', author='u2', timestamp=now + timedelta(seconds=4))
+        p3 = Post(body='post from mary', author='u3', timestamp=now + timedelta(seconds=3))
+        p4 = Post(body='post from david', author='u4', timestamp=now + timedelta(seconds=2))
         db.session.add_all([p1, p2, p3, p4])
         db.session.commit()
 
@@ -68,7 +66,18 @@ class UserModelCase(unittest.TestCase):
         u1.follow(u2) # john follows susan
         u1.follow(u4) # john follows david
         u2.follow(u3) # susan follows mary
-        u3.follow(u4) # marry follows david
+        u3.follow(u4) # mary follows david
         db.session.commit()
 
         # check the followed posts of each user
+        f1 = u1.followed_posts().all()
+        f2 = u2.followed_posts().all()
+        f3 = u3.followed_posts().all()
+        f4 = u4.followed_posts().all()
+        self.assertEqual(f1, [p2, p4, p1])
+        self.assertEqual(f2, [p2, p3])
+        self.assertEqual(f3, [p3, p4])
+        self.assertEqual(f4, [p4])
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
