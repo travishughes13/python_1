8570700d653675223a90fa74ab3c3ea7e9fd8d45
TREX Hughes <TREX@traviss-mbp-457.attlocal.net>
2019-07-20 20:36:35 -0500

    pushing changes

--- app/routes.py
+++ app/routes.py
@@ -106,3 +106,6 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('.follow/<username>')
+@login_required
